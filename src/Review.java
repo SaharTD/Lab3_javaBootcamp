96ca1cbdfd645509c5d629cda9b0e886ba6d0cb6
Sahar Turki <103968284+SaharTD@users.noreply.github.com>
2025-03-01 22:03:26 -0900

    Update Review.java

--- src/Review.java
+++ src/Review.java
@@ -1,11 +1,11 @@
 public class Review {
 
     private String username ;
-    private int rating ;
+    private double rating ;
     private String comment;
 
 
-    public Review(String comment, int rating, String username) {
+    public Review(String comment, double rating, String username) {
         this.comment = comment;
         this.rating = rating;
         this.username = username;
@@ -19,11 +19,11 @@ public class Review {
         this.comment = comment;
     }
 
-    public int getRating() {
+    public double getRating() {
         return rating;
     }
 
-    public void setRating(int rating) {
+    public void setRating(double rating) {
         this.rating = rating;
     }
 
@@ -33,5 +33,58 @@ public class Review {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "\nReview" +
+                "\nThe comment : " + comment +
+                "\n by username :" + username +
+                "\n with rating :" + rating;
+    }
+}
+public class Review {
+
+    private String username ;
+    private double rating ;
+    private String comment;
+
+
+    public Review(String comment, double rating, String username) {
+        this.comment = comment;
+        this.rating = rating;
+        this.username = username;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "\nReview" +
+                "\nThe comment : " + comment +
+                "\n by username :" + username +
+                "\n with rating :" + rating;
     }
 }
